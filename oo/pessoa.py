e1f8dd21e3c0ee8c2173632b035fa8824d1c522d
miltonbastos <miltonbastos@gmail.com>
2019-11-11 16:55:28 -0300

    Atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,22 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print((id(p)))
-    print(p.cumprimentar())
+    luciano = Pessoa(nome='Luciano')
+    milton = Pessoa(luciano, nome='Milton')
+    print(Pessoa.cumprimentar(milton))
+    print((id(milton)))
+    print(milton.cumprimentar())
+    # p.nome = 'Milton'
+    print(milton.nome)
+    print(milton.idade)
+    for filho in milton.filhos:
+        print(filho.nome)
 
 
 
